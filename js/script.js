9f5780ddf7a042feec7db80bd89274399d34443d
Fiona Wang1 <Fiona_Wang1@epam.com>
2016-02-25 11:33:03 +0800

    answer

--- js/script.js
+++ js/script.js
@@ -8,38 +8,50 @@ $("#prepend button").click(function() {
 });
 
 $("#before button").click(function() {
-	
+  $("#before p").before('ha! ');
+
 });
 
 $("#css1 button").click(function() {
-	
+  $("#css1 p").css('font-size','30px');
+
 });
 
 $("#css2 button").click(function() {
-	
+  $('#css2 p').css({"color": "red", "font-size": "20px"});
+
 });
 
 $("#attr1 button").click(function() {
-	
+  console.log("visit baidu?");
 });
 
 $("#attr2 button").click(function() {
+  $('#attr2 p').attr('p','visit baidu');
 
 });
 
 $("#class1 button").click(function() {
-	
+  $("#class1 p").addClass("bg-primary");
+
 });
 
 $("#class2 button").click(function() {
+  if(
+  $("#class2 p").hasClass( "lead")){
+    $("#class2 p").after('yes!');
+  }
 
 });
 
 $("#form1 button").click(function() {
+  var k = $("#exampleInputEmail1").val();
+  console.log(k);
 
 });
 
 $("#form2 button").click(function() {
+  var m = $("#exampleInputName2").val("fiona");
 
 });
 
